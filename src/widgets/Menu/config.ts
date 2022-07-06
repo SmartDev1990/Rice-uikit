11a7c5818047931c3946aae7b41be6a83818face
Smartdev <95247835+SmartDev1990@users.noreply.github.com>
2022-07-07 06:01:17 +0700

    Update config.ts
    Fix

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -120,7 +120,7 @@ export const socials = [
     items: [
       {
         label: "English",
-        href: "https://t.me/rice_pro,
+        href: "https://t.me/rice_pro",
       },
       {
         label: "Announcements",
